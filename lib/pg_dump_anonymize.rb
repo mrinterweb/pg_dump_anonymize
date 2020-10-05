0430b7ac64e4b3b0057edf77cead2e1e388478c0
Sean McCleary <seanmcc@gmail.com>
2020-10-04 22:41:22 -0700

    Initial commit

---
+++ lib/pg_dump_anonymize.rb
@@ -0,0 +1,6 @@
+require "pg_dump_anonymize/version"
+
+module PgDumpAnonymize
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pg_dump_anonymize/version.rb
@@ -0,0 +1,3 @@
+module PgDumpAnonymize
+  VERSION = "0.1.0"
+end
